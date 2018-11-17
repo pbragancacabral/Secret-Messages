fe43b412a79a6b897bc2d939ebeb0bc99ea396ac
Pedro de Bragança Cabral <pbragancacabral@gmail.com>
2018-11-17 16:44:34 +0100

    Add tests for encryption/decryption with Atbash

--- tests.py
+++ tests.py
@@ -1,8 +1,14 @@
 import unittest
 
+from atbash import Atbash
+
 
 class SecretMessagesTests(unittest.TestCase):
-    pass
+    def test_atbash_encryption(self):
+        return self.assertEqual(Atbash.encrypt("Encryption with Atbash"), "vMXIBKGRLM DRGS zGYZHS")
+
+    def test_atbash_decryption(self):
+        return self.assertEqual(Atbash.decrypt("wVXIBKGRLM DRGS zGYZHS"), "Decryption with Atbash")
 
 
 if __name__ == '__main__':
